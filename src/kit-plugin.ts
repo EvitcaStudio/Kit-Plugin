3cab956533382b2a76840312f1e3988adc179010
Active <56242467+doubleactii@users.noreply.github.com>
2025-01-01 03:59:05 -0600

    update: kit & add typing

--- src/kit-plugin.ts
+++ src/kit-plugin.ts
@@ -1,3 +1,4 @@
+import type { EventEmitter, EmitterEvent } from '@evitcastudio/kit'
 export abstract class KitPlugin {
     /**
      * The name of this plugin.
@@ -6,16 +7,12 @@ export abstract class KitPlugin {
     /**
      * The emitter that belongs to this plugin.
      */
-    protected _emitter: /*EventEmitter*/ unknown;
-
-    constructor() {
-
-    }
+    protected _emitter: EventEmitter | null = null;
 
     /**
      * Registers the plugin.
      */
-    _register(pEmitter: /*EventEmitter*/ unknown) {
+    _register(pEmitter: EventEmitter) {
         if (!this.name || typeof this.name !== 'string' || !/^[a-zA-Z0-9-_]+$/.test(this.name)) {
             throw new Error(`[Kit Plugin] Invalid plugin name: '${this.name}'. The name must be a non-empty string containing only alphanumeric characters, dashes, or underscores.`);
         }
@@ -32,7 +29,7 @@ export abstract class KitPlugin {
      * Emit an event.
      * @param pEvent - The event to emit.
      */
-    emit(pEvent: /*EmitterEvent*/ unknown) {
+    emit(pEvent: EmitterEvent) {
         if (!this._emitter) {
             console.error('[Kit Plugin] emitter not set. This is an indication that the plugin is not registered.');
             return;
